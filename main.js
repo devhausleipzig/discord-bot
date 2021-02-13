0338c3450f019c34d3b3b7644b7ca5f9fdf9bb6c
franzwollang <franzwollang@gmail.com>
2021-02-13 22:28:25 +0100

    Changed RegEx to accomodate words with apostrophes

--- main.js
+++ main.js
@@ -17,7 +17,10 @@ client.on("message", message => {
 
   if (message.author.id == victimID && !message.author.bot) {
 
-    const partial = message.content.match(/(?:im|i'm|i am)((?: \w+){1,4})/i)[1].trim();
+    const partial = message.content
+      .match(/(?:im|i'm|i am)((?: (?:\w+(?:'(?:re|[tdms]))?)){1,4})/i)[1]
+      .trim();
+
     if ( partial ) {
         message.channel.send(`Hi *${partial}*, I'm dad.`);
       }
