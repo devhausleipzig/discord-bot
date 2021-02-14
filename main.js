07d4477179d22754a6acbf4b139aad02f507992e
franzwollang <franzwollang@gmail.com>
2021-02-14 19:25:36 +0100

    Fixed incorrect victimID

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 let botAvatar, timeout;
 
-const victimID = '227127395268820992'; //simon
+const victimID = '665658548956561441'; //simon
 //const victimID = '305765240942100480'; //taylor
 //const victimID = '227127395268820992' //franz
 
